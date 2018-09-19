94aff029f7cb7ccb7efeb74422814a28e5a98b3a
czqzju <czqzju@gmail.com>
2018-09-19 14:29:50 +0800

    add code for getting links for houses

--- scratchHousePrice.py
+++ scratchHousePrice.py
@@ -1,4 +1,8 @@
+#coding: utf-8
+import time
+
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 
@@ -6,20 +10,37 @@ from selenium import webdriver
 class scratchHousePrice():
     def __init__(self):
         self.driver = webdriver.Chrome()
+        self.f = open("data\\link.txt", 'w')
 
 
     def scrtchHousePrice(self):
         driver = self.driver
+        file = self.f
         driver.get("https://hz.lianjia.com/ershoufang/")
-        content = self.findPageCotent(driver)
+        driver.maximize_window()
+        self.getLinks(driver, file)
 
-    def findPageCotent(self, driver):
+
+    def getLinks(self, driver, file):
+        while True:
+            self.findPageCotent(driver, file)
+            try:
+                nextPage = driver.find_element_by_link_text('下一页')
+            except NoSuchElementException as msg:
+                print(u'到达最后一页%s'%msg)
+                break
+            driver.execute_script("window.scrollBy(0,document.body.scrollHeight)", "")
+            nextPage.click()
+
+    def findPageCotent(self, driver, file):
         items = driver.find_elements_by_xpath("//li[@class = 'clear LOGCLICKDATA']/a")
-        print(len(items))
+        for item in items:
+            file.write(item.get_attribute('href')+'\n')
 
 
     def __del__(self):
         self.driver.close()
+        self.f.close()
 
 
 
