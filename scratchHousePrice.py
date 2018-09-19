decfaa0cb08a139c6978f3f1b827eb2481326107
czqzju <czqzju@gmail.com>
2018-09-19 09:31:50 +0800

    Initial commit

---
+++ scratchHousePrice.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+
+
+
+
+class scratchHousePrice():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+
+    def scrtchHousePrice(self):
+        driver = self.driver
+        driver.get("https://hz.lianjia.com/ershoufang/")
+        content = self.findPageCotent(driver)
+
+    def findPageCotent(self, driver):
+        items = driver.find_elements_by_xpath("//li[@class = 'clear LOGCLICKDATA']/a")
+        print(len(items))
+
+
+    def __del__(self):
+        self.driver.close()
+
+
+
+if __name__ == "__main__":
+    scratchDo = scratchHousePrice()
+    scratchDo.scrtchHousePrice()
